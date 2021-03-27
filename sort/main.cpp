646fa6c4c55e6cb88fb0f17a565e85b34fee0459
666 <1339713363@qq.com>
2021-03-27 16:07:30 +0800

    完成选择排序.

--- sort/main.cpp
+++ sort/main.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
 
 
-/*
-* 选择排序
-* 稳定, 时间复杂度O(n^2), 空间复杂度：O(1)
-* 
-*/
-void selectSort(int *arr, int length) {
-
+void swap(int& a, int& b) {
+    int tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
 }
 
-void printarr(int* arr, int length) {
-    for (int i = 0; i < length; i++) {
-        std::cout << arr[i] << "  " << std::endl;
+void printarr(int* arr, int len) {
+    for (int i = 0; i < len; i++) {
+        std::cout << arr[i] << "  ";
+    }
+    std::cout << std::endl;
+}
+
+/*
+* 选择排序
+* 遍历 len-1 次, 每次选择出最小的元素放到前面
+* 稳定, 时间复杂度O(n^2), 空间复杂度：O(1)
+*/
+void selectSort(int *arr, int len) {
+    for (int i = 0; i < len-1; i++) {
+        // 寻找最小元素
+        int min = i;
+        for (int j = i + 1; j < len; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        // 交换当前元素与最小元素
+        swap(arr[i], arr[min]);
     }
 }
 
 
 int main() {
     int arr[] = { 3, 2, 6, 8, 5, 1, 9, 7 };
-    int length = sizeof(arr) / sizeof(arr[0]);
+    int len = sizeof(arr) / sizeof(arr[0]);
 
     std::cout << "原数组\n";
-    printarr(arr, length);
+    printarr(arr, len);
 
+    selectSort(arr, len);
+    std::cout << "选择排序\n";
+    printarr(arr, len);
 
 }
